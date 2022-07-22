cc9bb897484e195b155ed4bdd0123e9d1b1c5f31
Vladimir Izmaylov <holybuddha@yandex.ru>
2022-07-22 17:49:22 +0300

    Added variable backgrounds

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -15,34 +15,52 @@ class ViewController: UIViewController {
     
     private lazy var locationLabel: UILabel = {
         let locationLabel = UILabel()
-        locationLabel.textColor = .white
-        locationLabel.font = locationLabel.font.withSize(40)
-        locationLabel.drawShadow(offset: CGSize(width: 5, height: 5), opacity: 0.1)
+        locationLabel.drawLabel(fontSize: 40)
+        locationLabel.drawShadow()
         return locationLabel
     }()
     
     private lazy var tempLabel: UILabel = {
         let tempLabel = UILabel()
-        tempLabel.textColor = .white
-        tempLabel.font = tempLabel.font.withSize(80)
-        tempLabel.drawShadow(offset: CGSize(width: 5, height: 5), opacity: 0.1)
+        tempLabel.drawLabel(fontSize: 80)
+        tempLabel.drawShadow()
         return tempLabel
     }()
     
     private lazy var weatherDescriptionLabel: UILabel  = {
         let weatherDescriptionLabel = UILabel()
-        weatherDescriptionLabel.textColor = .white
-        weatherDescriptionLabel.font = weatherDescriptionLabel.font.withSize(20)
-        weatherDescriptionLabel.drawShadow(offset: CGSize(width: 5, height: 5), opacity: 0.1)
+        weatherDescriptionLabel.drawLabel(fontSize: 20)
+        weatherDescriptionLabel.drawShadow()
         return weatherDescriptionLabel
+    }()
+    
+    private lazy var weatherIcon: UIImageView = {
+        let weatherIcon = UIImageView()
+        weatherIcon.drawShadow()
+        return weatherIcon
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis  = NSLayoutConstraint.Axis.vertical
+        stackView.distribution  = UIStackView.Distribution.equalSpacing
+        stackView.alignment = UIStackView.Alignment.center
+        stackView.spacing = 5
+        
+        stackView.addArrangedSubview(locationLabel)
+        stackView.addArrangedSubview(tempLabel)
+        stackView.addArrangedSubview(weatherDescriptionLabel)
+        stackView.addArrangedSubview(weatherIcon)
+        
+        return stackView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startLocationManager()
-        assignbackground()
-        setupSubviews(locationLabel, tempLabel, weatherDescriptionLabel)
+        //assignbackground()
+        setupSubviews(stackView)
         setConstraits()
     }
     
@@ -50,47 +68,65 @@ class ViewController: UIViewController {
         subviews.forEach { subview in
             view.addSubview(subview)
         }
-        
     }
     
     private func setConstraits() {
-        locationLabel.translatesAutoresizingMaskIntoConstraints = false
-        tempLabel.translatesAutoresizingMaskIntoConstraints = false
-        weatherDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+//        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+//        tempLabel.translatesAutoresizingMaskIntoConstraints = false
+//        weatherDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+//        weatherIcon.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            locationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            locationLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            tempLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            tempLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 5),
-            weatherDescriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            weatherDescriptionLabel.topAnchor.constraint(equalTo: tempLabel.bottomAnchor, constant: 5)
+            stackView.topAnchor.constraint(lessThanOrEqualTo: view.topAnchor, constant: 100),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/3)
+//            locationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            locationLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+//            locationLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/10),
+//
+//            tempLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            tempLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 5),
+//            //tempLabel.heightAnchor.constraint(equalTo: locationLabel.heightAnchor),
+//
+//            weatherDescriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            weatherDescriptionLabel.topAnchor.constraint(equalTo: tempLabel.bottomAnchor, constant: 5),
+//            //weatherDescriptionLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/10),
+//
+//            weatherIcon.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            weatherIcon.topAnchor.constraint(equalTo: weatherDescriptionLabel.bottomAnchor, constant: 5),
+//            //weatherIcon.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/10)
         ])
     }
     
     private func updateLabels() {
         locationLabel.text = weatherData?.name ?? "ERROR"
         tempLabel.text = checkTemp(weatherData?.main.temp ?? 0)
-        weatherDescriptionLabel.text = DataSource.weatherIDs[weatherData?.weather[0].id ?? 200]
-        //String(Int(weatherData?.main.temp ?? 0))
+        weatherDescriptionLabel.text = weatherConditions.weatherIDs[weatherData?.weather[0].id ?? 200]
+        weatherIcon.image = UIImage(named: weatherData?.weather[0].icon ?? "ERROR")
+        assignbackground()
     }
     
     private func checkTemp(_ temp: Double) -> String {
-        if temp > 0 { return "+" + "\(Int(temp))" + "\u{00B0}"
-        } else if temp == 0 {  return "0" + "\u{00B0}"
-        } else { return "+" + "\(Int(temp))" + "\u{00B0}"
+        if temp > 0 {
+            return "+" + "\(Int(temp))" + "\u{00B0}"
+        } else if temp == 0 {
+            return "0" + "\u{00B0}"
+        } else {
+            return "\(Int(temp))" + "\u{00B0}"
         }
     }
     
-    func assignbackground() {
+    private func assignbackground() {
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
-        backgroundImage.image = #imageLiteral(resourceName: "back_weather")
+        backgroundImage.image = updateBackgroundImage(id: weatherData?.weather[0].id ?? 200)
+        //#imageLiteral(resourceName: "darkCloudy")
         backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
         self.view.insertSubview(backgroundImage, at: 0)
     }
     
     
-    func startLocationManager() {
+    private func startLocationManager() {
         locationManager.requestWhenInUseAuthorization()
         
         if CLLocationManager.locationServicesEnabled() {
@@ -101,7 +137,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func updateWeatherInfo(latitude: Double, longitude: Double) {
+    private func updateWeatherInfo(latitude: Double, longitude: Double) {
         let urlWithCoordinates = "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude.description)&lon=\(longitude.description)&units=metric&lang=ru&appid=edfe94b1ee9b1f9ceecd7596d2f66b06"
         
         NetworkManager.shared.fetchData(from: urlWithCoordinates) { weatherData in
@@ -110,7 +146,19 @@ class ViewController: UIViewController {
             print(self.weatherData ?? "ERROR")
         }
     }
+    private func updateBackgroundImage(id: Int) -> UIImage {
+        var backgroundImage = UIImage(#imageLiteral(resourceName: "back_weather"))
+        if id >= 800 {
+            backgroundImage = UIImage(#imageLiteral(resourceName: "darkCloudy"))
+        } else if id < 300 {
+            backgroundImage = UIImage(#imageLiteral(resourceName: "storm"))
+        } else if id > 300 && id <= 531 {
+            backgroundImage = UIImage(#imageLiteral(resourceName: "rainy"))
+        }
+        return backgroundImage
+    }
 }
+
 
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -121,11 +169,20 @@ extension ViewController: CLLocationManagerDelegate {
 }
 
 extension UIView {
-    func drawShadow(offset: CGSize, opacity: Float = 0.25, color: UIColor = .black, radius: CGFloat = 1) {
-           layer.masksToBounds = false
-           layer.shadowColor = color.cgColor
-           layer.shadowOffset = offset
-           layer.shadowOpacity = opacity
-           layer.shadowRadius = radius
-       }
+    func drawShadow(offset: CGSize = CGSize(width: 5, height: 5), opacity: Float = 0.1, color: UIColor = .black, radius: CGFloat = 1) {
+        layer.shadowColor = color.cgColor
+        layer.shadowOffset = offset
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+    }
+}
+extension UILabel {
+    func drawLabel(textColor: UIColor = .white, fontSize: CGFloat, adjustsFontSizeToFitWidth: Bool = true, numberOfLines: Int = 0, minimumScaleFactor: CGFloat = 0.5, baselineAdjustment: UIBaselineAdjustment = .alignCenters, textAlignment: NSTextAlignment = .center) {
+        self.textColor = textColor
+        self.font = self.font.withSize(fontSize)
+        self.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth
+        self.numberOfLines = numberOfLines
+        self.minimumScaleFactor = minimumScaleFactor
+        self.baselineAdjustment = baselineAdjustment
+        self.textAlignment = textAlignment}
 }
