1d5427a696d54f1d8c908daeb125aad222668108
Vladimir Izmaylov <holybuddha@yandex.ru>
2022-08-16 14:05:28 +0300

    Added CollectionView

---
+++ WeatherApp/Views/CollectionView.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionView.swift
+//  WeatherApp
+//
+//  Created by Vladimir Izmaylov on 16.08.2022.
+//
+
+import UIKit
+
+class CollectionView: UICollectionView, UICollectionViewDelegate, UICollectionViewDataSource,
+                      UICollectionViewDelegateFlowLayout  {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    
+}
